935f13058f328b52b380d29341d5f56ecb2b199b
James Bennetts <jamesbennetts@gmail.com>
2019-03-12 12:24:39 +0000

    update

--- main.js
+++
@@ -1,8 +0,0 @@
-
-// When the user clicks on <div>, open the popup
-// function myFunction() {
-//   var phone-popup = document.getElementById("myPopup");
-//   phone-popup.classList.toggle("show");
-// }
-
-function myFunciton() { phone-popup=window.open("","PopUpWindow","width=100,height=50,top=10,left=20,resizable=no,scrollbars=no,menubar=no,toolbar=no,status=yes,location=no") }
