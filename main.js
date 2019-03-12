43ef3f93a0bb325513b94da8e9e58591e04259b0
James Bennetts <jamesbennetts@gmail.com>
2019-03-12 10:13:03 +0000

    update

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-<script>
+
 // When the user clicks on <div>, open the popup
-function myFunction() {
-  const popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
-</script>
+// function myFunction() {
+//   var phone-popup = document.getElementById("myPopup");
+//   phone-popup.classList.toggle("show");
+// }
+
+function myFunciton() { phone-popup=window.open("","PopUpWindow","width=100,height=50,top=10,left=20,resizable=no,scrollbars=no,menubar=no,toolbar=no,status=yes,location=no") }
